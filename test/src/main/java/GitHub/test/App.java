4b87eec02785d3f8b87cef85da5c57aab27a885c
Naw Aung <nawaung@ansurs.jp>
2019-09-12 10:51:39 +0900

    123

---
+++ test/src/main/java/GitHub/test/App.java
@@ -0,0 +1,13 @@
+package GitHub.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
